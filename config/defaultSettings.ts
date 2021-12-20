ec5c0c33f0e15497ecaaf400254d1e9c4701ff20
zhanggy <geodaoyu@foxmail.com>
2021-12-20 11:09:33 +0800

    chore: upload public/images

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -12,7 +12,7 @@ const Settings: LayoutSettings & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'ToDoList',
+  title: 'TodoList',
   pwa: false,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -8,47 +8,48 @@ interface ListItem {
   description: string;
 }
 
+const dataSource: ListItem[] = [
+  {
+    title: 'jQuery',
+    avatar: './images/jQuery.ico',
+    to: '/jQuery',
+    description: 'jQuery is a fast, small, and feature-rich JavaScript library.',
+  },
+  {
+    title: 'Ramda',
+    avatar: './images/Ramda.ico',
+    to: '/Ramda',
+    description: '一款实用的 JavaScript 函数式编程库。',
+  },
+  {
+    title: 'DvaJS',
+    avatar: './images/DvaJS.jpg',
+    to: '/DvaJS',
+    description:
+      'dva 首先是一个基于 redux 和 redux-saga 的数据流方案，然后为了简化开发体验，dva 还额外内置了 react-router 和 fetch，所以也可以理解为一个轻量级的应用框架。',
+  },
+  {
+    title: 'Redux Toolkit',
+    avatar: './images/RTK.ico',
+    to: '/RTK',
+    description:
+      'The official, opinionated, batteries-included toolset for efficient Redux development',
+  },
+  {
+    title: 'ahooks',
+    avatar: './images/ahooks.svg',
+    to: '/ahooks',
+    description: 'A high-quality & reliable React Hooks library',
+  },
+  {
+    title: 'rxjs',
+    avatar: './images/rxjs.ico',
+    to: '/rxjs',
+    description: 'Reactive Extensions Library for JavaScript',
+  },
+];
+
 export default () => {
-  const dataSource: ListItem[] = [
-    {
-      title: 'jQuery',
-      avatar: 'https://api.jquery.com/favicon.ico',
-      to: '/jQuery',
-      description: 'jQuery is a fast, small, and feature-rich JavaScript library.',
-    },
-    {
-      title: 'Ramda',
-      avatar: 'https://ramda.cn/favicon.ico',
-      to: '/Ramda',
-      description: '一款实用的 JavaScript 函数式编程库。',
-    },
-    {
-      title: 'DvaJS',
-      avatar: 'https://zos.alipayobjects.com/rmsportal/psagSCVHOKQVqqNjjMdf.jpg',
-      to: '/DvaJS',
-      description:
-        'dva 首先是一个基于 redux 和 redux-saga 的数据流方案，然后为了简化开发体验，dva 还额外内置了 react-router 和 fetch，所以也可以理解为一个轻量级的应用框架。',
-    },
-    {
-      title: 'Redux Toolkit',
-      avatar: 'https://redux-toolkit.js.org/img/favicon/favicon.ico',
-      to: '/RTK',
-      description:
-        'The official, opinionated, batteries-included toolset for efficient Redux development',
-    },
-    {
-      title: 'ahooks',
-      avatar: 'https://ahooks.js.org/simple-logo.svg',
-      to: '/ahooks',
-      description: 'A high-quality & reliable React Hooks library',
-    },
-    {
-      title: 'rxjs',
-      avatar: 'https://rxjs-dev.firebaseapp.com/assets/images/favicons/favicon.ico',
-      to: '/rxjs',
-      description: 'Reactive Extensions Library for JavaScript',
-    },
-  ];
   return (
     <List
       itemLayout="horizontal"
